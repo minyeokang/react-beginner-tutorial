da3f849e2511ff1645218a9141ffdaecfb636cdd
minyeokang <dobieborie1@gmail.com>
2023-04-12 16:45:06 +0900

    basic start up

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//pascalCasing - Capitalize the first letter of function
+
+function Message(){
+    const name = "minyeo"
+    if(name)
+        return <h1>hello {name}</h1>;
+    return <h1>hello world</h1>;
+}
+
+export default Message;
