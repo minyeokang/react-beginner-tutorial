40b73be35b6be2ab220e0927785b722414d74d4a
minyeokang <dobieborie1@gmail.com>
2023-04-12 16:53:23 +0900

    add bootstrap, using list-group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">an item</li>
+      <li className="list-group-item">an item</li>
+      <li className="list-group-item">an item</li>
+      <li className="list-group-item">an item</li>
+      <li className="list-group-item">an item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
