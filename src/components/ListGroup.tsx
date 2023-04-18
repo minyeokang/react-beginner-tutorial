987648887439a49c4f4ee4b34bdb6d4e71647ebf
minyeo0729 <minyeo@studio-jt.co.kr>
2023-04-18 16:16:41 +0900

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,35 @@
 function ListGroup() {
+    let items = [
+        'thailand',
+        'singapore',
+        'bali',
+        'vietnam',
+        'laos'
+    ]
+    items = []
   return (
-    <ul className="list-group">
-      <li className="list-group-item">an item</li>
-      <li className="list-group-item">an item</li>
-      <li className="list-group-item">an item</li>
-      <li className="list-group-item">an item</li>
-      <li className="list-group-item">an item</li>
+    <>
+    <h1>List</h1>
+     <ul className="list-group">
+      {items.length === 0 && <p>no item</p>}
+      {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
     </ul>
+    </>
+   
   );
 }
 
 export default ListGroup;
+
+/** conditional rendering explanation : 
+ * 
+ * if(items.length === 0) return <p>no item</p> 
+ * ==> items.length === 0 ? <p>no item</p> : null 
+ * ==> items.length === 0 && <p>no item</p>
+ * ex) true && 1 === 1 
+ * ex) false && 1 === false 
+ * if statement is 'true', it will return the next one 
+ * 
+ * you can also put things in function and use { } 
+ * ex) <ul> {message()} <li></li> </ul>
+ */
