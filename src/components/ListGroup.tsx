66c2eb90dd3be372bd8be319e088e8a12fa000e7
minyeo0729 <minyeo@studio-jt.co.kr>
2023-04-18 17:20:02 +0900

    useState hook

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
     let items = [
         'thailand',
@@ -6,13 +8,13 @@ function ListGroup() {
         'vietnam',
         'laos'
     ]
-    items = []
+    const [selectedIndex, setSelectedIndex ] = useState(-1)
   return (
     <>
     <h1>List</h1>
      <ul className="list-group">
       {items.length === 0 && <p>no item</p>}
-      {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+      {items.map((item, index)=> <li key={item} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={()=> setSelectedIndex(index)}>{item}</li>)}
     </ul>
     </>
    
@@ -20,6 +22,34 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
+/** useState hook
+ * 
+ * let selectedIndex = -1; // -1 means none of the item. 
+ * onClick={()=>selectedIndex = index}
+ * ==> this is not going to work. you need useState to update the variable's states! 
+ * 
+ * const arr = useState(-1) 
+ * ==> arr(0) //variable 
+ * ==> arr(1) //updater function 
+ * 
+ * const [selectedIndex, setSelectedIndex ] = useState(-1)
+ * onClick={()=> setSelectedIndex(index)
+ * 
+ * this will update the state of selectedIndex variable
+ * and this state stays in each ListGroup components 
+ * if you have another ListGroup componets, that state has its own state
+ */
+
+/** Typescript type annotation
+ * 
+ * onClick={(event)=>{console.log(event)}} 
+ * ==> const handleClick = (event: MouseEvent) => {console.log(event}
+ * have to import MouseEvent
+ * 
+ * once you define things like this, it's easy to use
+ * no need to console.log what event they have, event. will show list of events right in here 
+ */
 
 /** conditional rendering explanation : 
  * 
